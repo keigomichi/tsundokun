9ec93c4cd305c8eb25a9fde4139afbe311bf3c8f
keigomichi <hundredacrewood1165g7@gmail.com>
2023-06-13 00:23:06 +0900

    feat: local datasourceを実装

---
+++ app/src/main/java/com/example/tsundokun/data/local/TsundokuEntity.kt
@@ -0,0 +1,16 @@
+package com.example.tsundokun.data.local
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tsundoku")
+data class TsundokuEntity(
+    @PrimaryKey val id: String,
+    @ColumnInfo(name = "link") val link: String,
+    @ColumnInfo(name = "is_read") val isRead: Boolean,
+    @ColumnInfo(name = "is_favorite") val isFavorite: Boolean,
+    @ColumnInfo(name = "created_at") val createdAt: String,
+    @ColumnInfo(name = "update_at") val updatedAt: String,
+    @ColumnInfo(name = "deleted_at") val deletedAt: String,
+)
