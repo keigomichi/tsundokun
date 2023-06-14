d38312169415501efa53453636e2f49237458df0
Koyuki Izumi <g1920506@is.ocha.ac.jp>
2023-06-14 22:06:12 +0900

    Revert "StackScreen関数の引数をデフォルト引数に変更"
    This reverts commit 201df4d78b4da90b109c895c533b3a0fca46c9ed.

--- app/src/main/java/com/example/tsundokun/ui/stack/StackScreen.kt
+++ app/src/main/java/com/example/tsundokun/ui/stack/StackScreen.kt
@@ -8,7 +8,7 @@ import com.ramcosta.composedestinations.annotation.Destination
 
 @Destination
 @Composable
-fun StackScreen(url: Uri = Uri.parse("")) {
+fun StackScreen(url: Uri) {
     Column() {
         Text(text = "積読を追加")
         Text(text = url.toString())
