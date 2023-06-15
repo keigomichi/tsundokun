0040f8fcb63b7185a39330110023445b51e53dc4
kototo522 <huyejiben@gmail.com>
2023-06-15 22:53:23 +0900

    add: ログイン画面のUI実装

--- app/src/main/java/com/example/tsundokun/ui/registration/LoginScreen.kt
+++ app/src/main/java/com/example/tsundokun/ui/registration/LoginScreen.kt
@@ -1,26 +1,70 @@
 package com.example.tsundokun.ui.registration
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import io.github.jan.supabase.gotrue.mfa.FactorType.TOTP.value
 
 @OptIn(ExperimentalMaterial3Api::class)
+@Preview
 @Composable
 fun LoginScreen(){
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
     Surface(
-        modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background,
     ) {
-        Scaffold() { innerPadding ->
-            Column(modifier = Modifier.padding(innerPadding)) {
-
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxSize().padding(horizontal = 20.dp),
+        ) {
+            Text(text = "つんどくん", style = MaterialTheme.typography.titleLarge)
+            Spacer(modifier = Modifier.height(20.dp))
+            OutlinedTextField(
+                value = email,
+                onValueChange = { email = it },
+                label = { Text(text = "メールアドレス") },
+                modifier = Modifier.fillMaxWidth().padding(vertical = 5.dp)
+            )
+            OutlinedTextField(
+                value = password,
+                onValueChange = { password = it },
+                label = { Text(text = "パスワード") },
+                modifier = Modifier.fillMaxWidth().padding(vertical = 5.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
+            Button(
+                onClick = { /* ログイン処理を実行 */ },
+                modifier = Modifier.fillMaxWidth().padding(vertical = 20.dp)
+            ) {
+                Text(text = "ログイン")
             }
+            Text(text = "新規登録はこちら", style = MaterialTheme.typography.bodySmall, modifier = Modifier.clickable { /*TODO*/ })
+            Spacer(modifier = Modifier.height(30.dp))
+            Text(text = "または", modifier = Modifier.padding(vertical = 10.dp))
+            Text(text = "google認証でログイン", modifier = Modifier.padding(vertical = 0.dp))
         }
     }
 }
--- app/src/main/java/com/example/tsundokun/ui/registration/RegistrationScreen.kt
+++ app/src/main/java/com/example/tsundokun/ui/registration/RegistrationScreen.kt
@@ -9,8 +9,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.ramcosta.composedestinations.annotation.Destination
-import com.ramcosta.composedestinations.annotation.RootNavGraph
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
