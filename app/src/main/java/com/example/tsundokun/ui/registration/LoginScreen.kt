f98d7b981fcb3c759931240ef28a15e92bbcb346
kototo522 <huyejiben@gmail.com>
2023-06-15 23:18:39 +0900

    remote: 不要なimportの削除

--- app/src/main/java/com/example/tsundokun/ui/registration/LoginScreen.kt
+++ app/src/main/java/com/example/tsundokun/ui/registration/LoginScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,12 +21,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tsundokun.ui.destinations.RegistrationScreenDestination
-import com.example.tsundokun.ui.destinations.SettingScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
-import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import io.github.jan.supabase.gotrue.mfa.FactorType.TOTP.value
 
--- app/src/main/java/com/example/tsundokun/ui/registration/RegistrationScreen.kt
+++ app/src/main/java/com/example/tsundokun/ui/registration/RegistrationScreen.kt
@@ -21,11 +21,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tsundokun.ui.destinations.LoginScreenDestination
-import com.example.tsundokun.ui.destinations.RegistrationScreenDestination
-import com.example.tsundokun.ui.destinations.SettingScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
