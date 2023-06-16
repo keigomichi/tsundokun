433c7e026b0131674c3a678fba5a713daf8dd413
sisosyunya <syunyasisotto@gmail.com>
2023-06-16 15:59:30 +0900

    fix

--- app/src/main/java/com/example/tsundokun/data/local/TsundokunDatabase.kt
+++ app/src/main/java/com/example/tsundokun/data/local/TsundokunDatabase.kt
@@ -4,8 +4,9 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.tsundokun.data.local.category.CategoryDao
 import com.example.tsundokun.data.local.category.CategoryEntity
+import com.example.tsundokun.data.local.tsundokucategory.TsundokuCategoryEntity
 
-@Database(entities = [TsundokuEntity::class, CategoryEntity::class], version = 1)
+@Database(entities = [TsundokuEntity::class, CategoryEntity::class,TsundokuCategoryEntity::class], version = 1)
 abstract class TsundokunDatabase : RoomDatabase() {
     abstract fun tsundokuDao(): TsundokuDao
     abstract fun categoryDao(): CategoryDao
