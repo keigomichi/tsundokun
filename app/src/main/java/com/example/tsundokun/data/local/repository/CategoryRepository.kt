ee6ee8b27b166e2af5af15c81b6ce7b2d84a4ebb
佐藤 佑哉 <y-sato@sstn.jp>
2023-08-31 00:20:46 +0900

    add interface CategoryRepository

---
+++ app/src/main/java/com/example/tsundokun/data/local/repository/CategoryRepository.kt
@@ -0,0 +1,10 @@
+package com.example.tsundokun.data.local.repository
+
+import com.example.tsundokun.domain.models.Category
+import kotlinx.coroutines.flow.Flow
+
+interface CategoryRepository {
+    fun observeAllCategory(): Flow<List<Category>>
+    suspend fun addCategory(category: Category)
+    suspend fun initializeDatabaseWithDefaultData()
+}
