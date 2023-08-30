65e2b274c2e8f3bff773b79933a3fd037908cec0
佐藤 佑哉 <y-sato@sstn.jp>
2023-08-31 00:20:24 +0900

    add method toCategory to CategoryEntity

--- app/src/main/java/com/example/tsundokun/data/local/entities/CategoryEntity.kt
+++ app/src/main/java/com/example/tsundokun/data/local/entities/CategoryEntity.kt
@@ -3,10 +3,27 @@ package com.example.tsundokun.data.local.entities
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.example.tsundokun.domain.models.Category
 
 @Entity(tableName = "category")
 data class CategoryEntity(
     @PrimaryKey val id: String,
     @ColumnInfo(name = "label") val label: String,
     @ColumnInfo(name = "created_at") val createdAt: Long,
-)
+) {
+    fun toCategory() = Category(
+        id = id,
+        label = label,
+        createdAt = createdAt,
+    )
+}
+
+fun List<CategoryEntity>.toCategory(): List<Category> {
+    return map {
+        Category(
+            id = it.id,
+            label = it.label,
+            createdAt = it.createdAt,
+        )
+    }
+}
