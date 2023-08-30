7a54d8da700d35abada92fb08759205ca9c2d386
佐藤 佑哉 <y-sato@sstn.jp>
2023-08-31 00:41:20 +0900

    add repository module

---
+++ app/src/main/java/com/example/tsundokun/data/repository/RepositoryModule.kt
@@ -0,0 +1,17 @@
+package com.example.tsundokun.data.repository
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindCategoryRepository(
+        defaultCategoryRepository: DefaultCategoryRepository
+    ): CategoryRepository
+}
