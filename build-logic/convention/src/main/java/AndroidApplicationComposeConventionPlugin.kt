3570af368a4700c4a6f64a0748e315bbd59eca9a
dao0203 <youzaizuoteng872@gmail.com>
2024-02-09 00:44:41 +0900

    add lifecycle compose ext

--- build-logic/convention/src/main/java/AndroidApplicationComposeConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidApplicationComposeConventionPlugin.kt
@@ -1,8 +1,10 @@
 import com.android.build.api.dsl.ApplicationExtension
 import com.example.tsundokun.buildlogic.configureAndroidCompose
+import com.example.tsundokun.buildlogic.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
 
 class AndroidApplicationComposeConventionPlugin: Plugin<Project> {
     override fun apply(target: Project) {
@@ -14,6 +16,12 @@ class AndroidApplicationComposeConventionPlugin: Plugin<Project> {
             extensions.configure<ApplicationExtension> {
                 configureAndroidCompose(this)
                 defaultConfig.targetSdk = 33
+            }
+
+            dependencies {
+                add("implementation", libs.findLibrary("androidx.hilt.navigation.compose").get())
+                add("implementation", libs.findLibrary("androidx.lifecycle.viewmodel.compose").get())
+                add("implementation", libs.findLibrary("androidx.lifecycle.runtime.compose").get())
             }
         }
     }
