ca3f3232251a69a06477a689297970c64e1f66bb
dao0203 <youzaizuoteng872@gmail.com>
2024-02-08 17:10:48 +0900

    set custom plugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -23,3 +23,40 @@ dependencies {
     compileOnly(libs.room.gradlePlugin)
     compileOnly(libs.ksp.gradlePlugin)
 }
+
+gradlePlugin{
+    plugins{
+        register("androidApplicationCompose") {
+            id = "tsundokun.android.application.compose"
+            implementationClass = "AndroidApplicationComposeConventionPlugin"
+        }
+        register("androidApplication"){
+            id = "tsundokun.android.application"
+            implementationClass = "AndroidApplicationConventionPlugin"
+        }
+        register("androidLibraryCompose"){
+            id = "tsundokun.android.library.compose"
+            implementationClass = "AndroidLibraryComposeConventionPlugin"
+        }
+        register("androidLibrary"){
+            id = "tsundokun.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
+        register("androidFeature"){
+            id = "tsundokun.android.feature"
+            implementationClass = "AndroidFeatureConventionPlugin"
+        }
+        register("androidHilt"){
+            id = "tsundokun.android.hilt"
+            implementationClass = "AndroidHiltConventionPlugin"
+        }
+        register("androidRoom"){
+            id = "tsundokun.android.room"
+            implementationClass = "AndroidRoomConventionPlugin"
+        }
+        register("androidTest"){
+            id = "tsundokun.android.test"
+            implementationClass = "AndroidTestConventionPlugin"
+        }
+    }
+}
